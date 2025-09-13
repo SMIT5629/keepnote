f2710bc89f552d01cf9512c14d611e1050d07469
Smit Patel <159561033+SMIT5629@users.noreply.github.com>
2025-09-13 16:52:02 +0530

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -11,11 +11,9 @@ const app = express();
 dotenv.config();
 // app.use(cors());
 app.use(cors({
-<<<<<<< HEAD
     origin: 'http://localhost:5173', // or your frontend URL
     credentials: true
-}));
-=======
+
     origin: 'https://keepnote-client.onrender.com', 
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
@@ -25,7 +23,6 @@ app.use(cors({
 app.options('*', cors());
 console.log('ENV:', process.env);
 
->>>>>>> 5982ca971e45a63ebc7d47a2b8c59e1ef4ea2ceb
 app.use(express.json());
 // connectDB();
 
